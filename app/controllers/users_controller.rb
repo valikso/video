00176a55a81d329307b038346bc56c0fffc3aa63
Your Name <you@example.com>
2017-10-26 14:30:23 +0300

    edit page

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,5 @@
 class UsersController < ApplicationController
+#  before_action :set_user, only: [:edit]
   def new
     @user = User.new
   end
@@ -20,7 +21,25 @@ class UsersController < ApplicationController
     end
   end
 
+  def edit
+    @user = User.find(params[:id])
+  end
+
+  def update
+    @user = User.find(params[:id])
+    if @user.update_attributes(user_params)
+      flash[:success] = 'Profile updated'
+      redirect_to @user
+    else
+      render 'edit'
+    end
+  end
+
 private
+
+def set_user
+
+end
 
 def user_params
   params.require(:user).permit(:name, :email, :password,
