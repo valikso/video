d8be8a1460ef7bc16884fa0b92b354de6aa687c7
Your Name <you@example.com>
2017-11-06 14:48:47 +0200

    ok

--- app/helpers/comments_helper.rb
+++ app/helpers/comments_helper.rb
@@ -1,2 +1,6 @@
 module CommentsHelper
+  def urls_to_images(s)
+     s.gsub! /\s(http:\/\/.*?\.(png|jpg))\s/, '<p><img src="\1" width=200px/></p>'
+     s.html_safe
+   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -15,21 +15,6 @@ ActiveRecord::Schema.define(version: 20171103215651) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
-  create_table "categories", force: :cascade do |t|
-    t.string "name"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-  end
-
-  create_table "categories_js", force: :cascade do |t|
-    t.bigint "movie_id"
-    t.bigint "category_id"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.index ["category_id"], name: "index_categories_js_on_category_id"
-    t.index ["movie_id"], name: "index_categories_js_on_movie_id"
-  end
-
   create_table "comments", force: :cascade do |t|
     t.integer "user_id"
     t.bigint "movie_id"
@@ -45,7 +30,7 @@ ActiveRecord::Schema.define(version: 20171103215651) do
     t.string "author"
     t.string "country"
     t.integer "year"
-    t.string "category"
+    t.string "genre"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "logo_file_name"
@@ -100,8 +85,6 @@ ActiveRecord::Schema.define(version: 20171103215651) do
     t.index ["remember_token"], name: "index_users_on_remember_token"
   end
 
-  add_foreign_key "categories_js", "categories"
-  add_foreign_key "categories_js", "movies"
   add_foreign_key "comments", "movies"
   add_foreign_key "taggings", "movies"
   add_foreign_key "taggings", "tags"
